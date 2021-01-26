814623791a1f69b0a6d554a58a27be67bb45f103
Lophiester <gilcharles2022@hotmail.com>
2021-01-26 13:10:56 +0900

    implementando DTO na categoria

--- src/main/java/com/lophiester/springmvc/resources/CategoriaResource.java
+++ src/main/java/com/lophiester/springmvc/resources/CategoriaResource.java
@@ -1,6 +1,7 @@
 package com.lophiester.springmvc.resources;
 
 import com.lophiester.springmvc.domain.Categoria;
+import com.lophiester.springmvc.dto.CategoriaDTO;
 import com.lophiester.springmvc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -19,6 +22,12 @@ public class CategoriaResource {
     public ResponseEntity<Categoria> findById(@PathVariable Integer id) {
         Categoria obj = categoriaService.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+    @GetMapping
+    public ResponseEntity<List<CategoriaDTO>>list(){
+        List<Categoria>list= categoriaService.findAll();
+        List<CategoriaDTO>listDTO= list.stream().map(categoria -> new CategoriaDTO(categoria)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 
     @PostMapping
--- src/main/java/com/lophiester/springmvc/services/CategoriaService.java
+++ src/main/java/com/lophiester/springmvc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import com.lophiester.springmvc.repositories.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -15,6 +16,10 @@ public class CategoriaService {
     public Categoria findById(Integer id) {
         Optional<Categoria> obj = categoriaRepository.findById(id);
         return obj.orElseThrow(null);
+    }
+
+    public List<Categoria> findAll() {
+        return categoriaRepository.findAll();
     }
 
     public Categoria insert(Categoria obj) {
